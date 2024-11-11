931f44177f98142e5261f3748b43b79ba097af92
taneem <muhammad.taneem@televersant.com>
2024-11-11 16:35:05 +0600

    changing on google auth

--- server/custom_user/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    is_verified = models.BooleanField(default=False)
